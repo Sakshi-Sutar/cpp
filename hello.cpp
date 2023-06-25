782df98c8c74428750f07be59ea6350a0a7e0856
Sakshi Sutar <53054910+Sakshi-Sutar@users.noreply.github.com>
2023-06-25 17:20:58 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Hello World!"<<endl;
+
+	return 0;
+}
+
+
+/* OUTPUT
+
+C:\Users\Sakshi Sutar\Documents\codes\cpp>g++ -o hello hello.cpp
+
+C:\Users\Sakshi Sutar\Documents\codes\cpp>hello
+Hello World!
+
+C:\Users\Sakshi Sutar\Documents\codes\cpp>
+
+*/
